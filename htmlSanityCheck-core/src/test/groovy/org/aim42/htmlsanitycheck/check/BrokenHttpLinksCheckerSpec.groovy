99d3ffc210e3744581c1214c130e1a4f76492189
Gerd Aschemann <gerd@aschemann.net>
2024-02-07 19:26:29 +0100

    WIP JReleaser

--- htmlSanityCheck-core/src/test/groovy/org/aim42/htmlsanitycheck/check/BrokenHttpLinksCheckerSpec.groovy
+++ htmlSanityCheck-core/src/test/groovy/org/aim42/htmlsanitycheck/check/BrokenHttpLinksCheckerSpec.groovy
@@ -154,6 +154,7 @@ class BrokenHttpLinksCheckerSpec extends Specification {
     }
 
 
+    @Ignore
     def "bad amazon link is identified as problem"() {
 
         given: "an HTML page with a single (good) amazon link"
